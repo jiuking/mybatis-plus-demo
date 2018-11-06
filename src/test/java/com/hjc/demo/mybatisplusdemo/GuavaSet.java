f46ac72c6013985c0f71503fa6bdef4a8703886e
hjc <lingshao2010@126.com>
2018-11-06 21:23:48 +0800

    set转换为list，saveOrUpdate中，需要保留id，所以，list原元素修改list信审元素

--- src/test/java/com/hjc/demo/mybatisplusdemo/GuavaSet.java
+++ src/test/java/com/hjc/demo/mybatisplusdemo/GuavaSet.java
@@ -1,5 +1,7 @@
 package com.hjc.demo.mybatisplusdemo;
 
+import com.google.common.collect.Collections2;
+import com.google.common.collect.Lists;
 import com.google.common.collect.Sets;
 
 import java.text.MessageFormat;
@@ -12,12 +14,12 @@ import java.util.stream.Collectors;
 public class GuavaSet {
     public static void main(String[] args) {
         List<Test.Student> list1 = new ArrayList<Test.Student>(){{
-            add(new Test.Student("Linda1",20,"男","1"));
+            add(new Test.Student("Linda",20,"男","1"));
             add(new Test.Student("Bruce1",20,"女",""));
-            add(new Test.Student("Linda",21,"",""));
+            add(new Test.Student("Linda",21,"男",""));
         }};
         List<Test.Student> list2 = new ArrayList<Test.Student>(){{
-            add(new Test.Student("Linda",21,"女",""));
+            add(new Test.Student("Linda1",21,"女",""));
             add(new Test.Student("Bruce",20,"女",""));
             add(new Test.Student("Jack",21,"",""));
         }};
@@ -26,7 +28,34 @@ public class GuavaSet {
 //        list.retainAll(list)  取出存在的id，设置到新的对象中。
         list1.stream().filter(temp -> list2.contains(temp)).collect(Collectors.toList());
 
+        //更新存在的id，
+        list1 = Lists.transform(list1,(input)->{
+            list2.forEach(temp ->{
+                if (temp.equals(input)) {
+                    input.setAge(temp.getAge());
+                    System.out.println("相等");
+                }
+            });
+            System.out.println(input);
+            return input;
+        });
+
+        System.out.println(list1);
+
         Sets.difference(new HashSet<>(), new HashSet<>());
+
+        Set<Integer> set1=Sets.newHashSet(1,2,3,4,5,6);
+        Set<Integer> set2=Sets.newHashSet(3,4,5,6,7,8,9);
+
+        //set 转换list
+        List list3 = new ArrayList(set1);
+        list3.forEach(System.out::println);
+
+        System.out.println("交集为：");//7,8,9
+        Set<Integer>S=Sets.difference(set2, set1);
+        for(Integer temp:S) {
+            System.out.println(temp);
+        }
     }
 
     public static class Student{
